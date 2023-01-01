d86480570778bd07742ad1ca2127c36727019fed
Sam Scheck <117218365+samcscheck@users.noreply.github.com>
2023-01-01 16:00:37 -0500

    Add files via upload

---
+++ RoyalGameOfUr/royal_game_of_ur.py
@@ -0,0 +1,379 @@
+"""
+File:    royal_game_of_ur.py
+Author:  Sophie Scheck
+Date:    11/1/2020
+Section: 42
+E-mail:  sscheck1@umbc.edu
+Description:
+  This program runs an ancient two player game called
+  the Royal Game of Ur
+"""
+
+from sys import argv
+from random import choice
+from board_square import BoardSquare, UrPiece
+
+
+WHITE = 'White'
+BLACK = 'Black'
+OFF = 'Off'
+COMPLETE = 'Complete'
+
+
+class RoyalGameOfUr:
+    STARTING_PIECES = 7
+
+    def __init__(self, board_file_name):
+        """
+        SHOULD HAVE MORE CODE
+        """
+        self.board = None
+        self.load_board(board_file_name)
+        # these are dictionaries to keep track of the individual player info
+        self.white = {'player': None, 'pieces': [], 'start piece': None, 'end piece': None}
+        self.black = {'player': None, 'pieces': [], 'start piece': None, 'end piece': None}
+        self.current_player = None
+        self.current_pieces = None
+
+    def load_board(self, board_file_name):
+        """
+        This function takes a file name and loads the map, creating BoardSquare objects in a grid.
+
+        :param board_file_name: the board file name
+        :return: sets the self.board object within the class
+        """
+
+        import json
+        try:
+            with open(board_file_name) as board_file:
+                board_json = json.loads(board_file.read())
+                self.num_pieces = self.STARTING_PIECES
+                self.board = []
+                for x, row in enumerate(board_json):
+                    self.board.append([])
+                    for y, square in enumerate(row):
+                        self.board[x].append(BoardSquare(x, y, entrance=square['entrance'], _exit=square['exit'], rosette=square['rosette'], forbidden=square['forbidden']))
+
+                for i in range(len(self.board)):
+                    for j in range(len(self.board[i])):
+                        if board_json[i][j]['next_white']:
+                            x, y = board_json[i][j]['next_white']
+                            self.board[i][j].next_white = self.board[x][y]
+                        if board_json[i][j]['next_black']:
+                            x, y = board_json[i][j]['next_black']
+                            self.board[i][j].next_black = self.board[x][y]
+        except OSError:
+            print('The file was unable to be opened. ')
+
+    def draw_block(self, output, i, j, square):
+        """
+        NEVER HAS TO BE CALLED
+        Helper function for the display_board method
+        :param output: the 2d output list of strings
+        :param i: grid position row = i
+        :param j: grid position col = j
+        :param square: square information, should be a BoardSquare object
+        """
+        MAX_X = 8
+        MAX_Y = 5
+        for y in range(MAX_Y):
+            for x in range(MAX_X):
+                if x == 0 or y == 0 or x == MAX_X - 1 or y == MAX_Y - 1:
+                    output[MAX_Y * i + y][MAX_X * j + x] = '+'
+                if square.rosette and (y, x) in [(1, 1), (1, MAX_X - 2), (MAX_Y - 2, 1), (MAX_Y - 2, MAX_X - 2)]:
+                    output[MAX_Y * i + y][MAX_X * j + x] = '*'
+                if square.piece:
+                    # print(square.piece.symbol)
+                    output[MAX_Y * i + 2][MAX_X * j + 3: MAX_X * j + 5] = square.piece.symbol
+
+    def display_board(self):
+        """
+        Draws the board contained in the self.board object
+        """
+        if self.board:
+            output = [[' ' for _ in range(8 * len(self.board[i//5]))] for i in range(5 * len(self.board))]
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if not self.board[i][j].forbidden:
+                        self.draw_block(output, i, j, self.board[i][j])
+
+            print('\n'.join(''.join(output[i]) for i in range(5 * len(self.board))))
+
+    def roll_d4_dice(self, n=4):
+        """
+        Keep this function as is.  It ensures that we'll have the same runs with different random seeds for rolls.
+        :param n: the number of tetrahedral d4 to roll, each with one dot on
+        :return: the result of the four rolls.
+        """
+        dots = 0
+        for _ in range(n):
+            dots += choice([0, 1])
+        return dots
+
+    def play_game(self):
+        """
+        This function is where the game functions are organized and ordered
+        """
+        self.create_players()
+        self.create_pieces()
+        self.set_start_and_end()
+
+        # This loop runs while a player has not won the game
+        end_game = False
+        turns_count = 0
+        self.display_board()
+        while not end_game:
+            turns_count += 1
+            if turns_count % 2 != 0:
+                self.current_player = self.white['player']
+                self.current_pieces = self.white['pieces']
+            else:
+                self.current_player = self.black['player']
+                self.current_pieces = self.black['pieces']
+            if turns_count > 1:
+                self.display_board()
+            roll = self.roll_d4_dice()
+            print(self.current_player, 'you rolled a', str(roll))
+            self.display_pieces(roll)
+            self.take_turn(roll)
+            end_game = self.end_game_flag()
+        if self.current_player == self.white['player']:
+            print('Congratulations,', self.white['player'], ', you have won the game!!')
+        else:
+            print('Congratulations,', self.black['player'], ', you have won the game!!')
+
+    def create_players(self):
+        """
+            This function prompts the players for names and adds them to their respective dictionary
+        """
+        self.white['player'] = input('What is your name? ')
+        print(self.white['player'], 'you will play as white.')
+        self.black['player'] = input('What is your name? ')
+        print(self.black['player'], 'you will play as black.')
+
+    def create_pieces(self):
+        """
+            This function creates the number of pieces set as the constant
+            START_PIECES and gives each piece a color and sybmol as an UrPiece object
+        """
+        for x in range(self.STARTING_PIECES):
+            piece_name = 'W' + str(x + 1)
+            piece = UrPiece(WHITE, piece_name)
+            self.white['pieces'].append(piece)
+        for x in range(self.STARTING_PIECES):
+            piece_name = 'B' + str(x + 1)
+            piece = UrPiece(BLACK, piece_name)
+            self.black['pieces'].append(piece)
+
+    def set_start_and_end(self):
+        """
+            This function establishes the start and end pieces for each color
+        """
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if self.board[x][y].entrance == WHITE:
+                    self.white['start piece'] = self.board[x][y]
+                if self.board[x][y].entrance == BLACK:
+                    self.black['start piece'] = self.board[x][y]
+                if self.board[x][y].exit == WHITE:
+                    self.white['end piece'] = self.board[x][y]
+                if self.board[x][y].exit == BLACK:
+                    self.black['end piece'] = self.board[x][y]
+
+    def take_turn(self, roll):
+        """
+            This function allows each player to choose a piece to move
+        :param roll: the number of moves the player has
+        :return:
+        """
+        piece = None
+        finished = False
+        extra_turn = False
+        # this loop runs until the player has entered a valid integer which can be moved
+        while not finished:
+            # the boolean extra_turn takes care of the rosette case when the player gains a turn
+            if extra_turn:
+                extra_turn = False
+                roll = self.roll_d4_dice()
+                print(self.current_player, 'you rolled a', str(roll))
+                self.display_pieces(roll)
+            can_move_count = 0
+            # this for loop counts the number of pieces which can be potentially moved
+            # with the number of moves they have rolled
+            for x in range(self.STARTING_PIECES):
+                check_piece = self.current_pieces[x]
+                if check_piece:
+                    if not check_piece.position:
+                        if self.current_player == self.white['player']:
+                            check_piece.start_pos = self.white['start piece']
+                        else:
+                            check_piece.start_pos = self.black['start piece']
+                if self.current_pieces[x].can_move(roll):
+                    can_move_count += 1
+            # if no pieces can be moved, the players turn is over
+            if can_move_count == 0:
+                finished = True
+                print('No moves are possible with the current dice roll.')
+            elif roll == 0:
+                finished = True
+                print('No moves are possible with the current dice roll.')
+            else:
+                moving_piece = input('Which move do you wish to make? ')
+                # these loops find which piece the player wants to move based on its symbol
+                for x in range(self.STARTING_PIECES):
+                    for y in range(self.STARTING_PIECES):
+                        if self.current_pieces[x].symbol[1] == str(y + 1) and str(y + 1) == moving_piece \
+                                and not self.current_pieces[x].complete:
+                            piece = self.current_pieces[x]
+                if piece:
+                    # if the piece does not have a position, this statement sets its position to the start
+                    if not piece.position:
+                        if self.current_player == self.white['player']:
+                            piece.start_pos = self.white['start piece']
+                        else:
+                            piece.start_pos = self.black['start piece']
+                    if not piece.can_move(roll):
+                        print('No moves are possible for this piece.')
+                    else:
+                        # if move returns True, the player has landed on a rosette, so they get another turn
+                        if not self.move(roll, piece):
+                            finished = True
+                        else:
+                            extra_turn = True
+
+    def display_pieces(self, num_moves):
+        """
+            This function displays the potential pieces that the player can move based on the
+            number of moves that they rolled
+        :param num_moves: this is the players roll
+        """
+        status = ''
+        for x in range(self.STARTING_PIECES):
+            piece = self.current_pieces[x]
+            # this sets the piece's start position
+            if piece:
+                if not piece.position:
+                    if self.current_player == self.white['player']:
+                        piece.start_pos = self.white['start piece']
+                    else:
+                        piece.start_pos = self.black['start piece']
+            if piece.can_move(num_moves):
+                # if can_move returns True, this statement checks to see if the piece is on or off the board
+                if not piece.position and not piece.complete:
+                    status = 'currently off the board'
+                elif not piece.complete:
+                    status = str(piece.position.position)
+                print(str(x + 1), piece.symbol, status)
+        # this for loop checks for pieces that have already completed the race
+        for x in range(self.STARTING_PIECES):
+            piece = self.current_pieces[x]
+            if piece:
+                if piece.complete:
+                    print(piece.symbol, 'has completed the race.')
+
+    def move(self, roll, piece):
+        """
+            This function moves the piece the player has selected to its new position on the board
+        :param roll: number of moves the player has
+        :param piece: the UrPiece which the player has selected to move
+        :return: True or False based on if the player has landed on a rosette
+        """
+        current_square = piece.position
+        on_board = False
+        if current_square:
+            on_board = True
+        # if the piece is not on the board, this places the pieces on the start piece
+        # and subtracts from their roll by 1
+        if not current_square and not on_board:
+            if self.current_player == self.white['player']:
+                current_square = self.white['start piece']
+            else:
+                current_square = self.black['start piece']
+            roll -= 1
+        # this for loops runs for the amount of moves a player has
+        for x in range(roll):
+            if self.current_player == self.white['player']:
+                # if the piece is on the last square of the board, the piece's position is changed to None
+                if current_square == self.white['end piece']:
+                    piece.position.piece = None
+                    piece.position = None
+                    piece.complete = True
+                # if the piece lands on a piece of a different color and it is not on a rosette,
+                # it knocks off the other piece and is put in its place
+                elif current_square.next_white.piece and current_square.next_white.piece.color != piece.color and \
+                        not current_square.next_white.rosette and roll == x + 1:
+                    print(current_square.next_white.piece.symbol, 'has been knocked off')
+                    current_square.next_white.piece.position = None
+                    current_square = current_square.next_white
+                # if the piece lands on an empty rosette, the player gets another turn
+                # and True is returned so that the player gets another roll within take_turn()
+                elif current_square.next_white.rosette and not current_square.next_white.piece and roll == x + 1:
+                    current_square = current_square.next_white
+                    if piece.position:
+                        piece.position.piece = None
+                    piece.position = current_square
+                    current_square.piece = piece
+                    self.display_board()
+                    print('You have landed on a rosette. You get another turn!')
+                    return True
+                # otherwise, the piece advances to the next square
+                else:
+                    current_square = current_square.next_white
+            # same code for black piece
+            else:
+                if current_square == self.black['end piece']:
+                    piece.position.piece = None
+                    piece.position = None
+                    piece.complete = True
+                elif current_square.next_black.piece and current_square.next_black.piece.color != piece.color and \
+                        not current_square.next_black.rosette and roll == x + 1:
+                    print(current_square.next_black.piece.symbol, 'has been knocked off')
+                    current_square.next_black.piece.position = None
+                    current_square = current_square.next_black
+                elif current_square.next_black.rosette and not current_square.next_black.piece and roll == x + 1:
+                    current_square = current_square.next_black
+                    if piece.position:
+                        piece.position.piece = None
+                    piece.position = current_square
+                    current_square.piece = piece
+                    self.display_board()
+                    print('You have landed on a rosette. You get another turn!')
+                    return True
+                else:
+                    current_square = current_square.next_black
+        # this code makes sure that the old BoardSquare does not still have the piece on it,
+        # because it has been changed to a new square
+        if piece.position and not piece.complete:
+            piece.position.piece = None
+        # this code changes the actual piece.position and current_square.piece
+        # to where the piece has been moved to
+        if current_square and not piece.complete:
+            piece.position = current_square
+            current_square.piece = piece
+
+    def end_game_flag(self):
+        """
+            This function checks to see if the all of the player's pieces have set piece.complete to True
+        :return: True or False if all of the player's pieces have completed the race
+        """
+        count = 0
+        # this code counts the number of pieces which have not completed the race
+        if self.current_player == self.white['player']:
+            for x in range(self.STARTING_PIECES):
+                if not self.white['pieces'][x].complete:
+                    count += 1
+        if self.current_player == self.black['player']:
+            for x in range(self.STARTING_PIECES):
+                if not self.black['pieces'][x].complete:
+                    count += 1
+        # if the count is 0, then none of the pieces have yet to finish the race and the player has won
+        if count == 0:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    file_name = input('What is the file name of the board json? ') if len(argv) < 2 else argv[1]
+    rgu = RoyalGameOfUr(file_name)
+    rgu.play_game()
---
+++ WordGuess/proj1.cpp
@@ -0,0 +1,250 @@
+/*                                     
+** Title: proj1.cpp                                                
+** Author: Sam Scheck      
+** Email: sscheck1@umbc.edu                                   
+** Date: 2/13/2021                                   
+** Description: This program allows the user to play hangman against the computer.
+**              This file contains the main driver as well as all of the functions
+**              required to run the program. 
+** Course/Section: CMSC 202/13                        
+*/
+
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+const int NUM_WRONG = 6;  // specified number of wrong guesses before game ends
+const int ASCII_VAL = 97;  // this ASCII value will be used to indicate lower case letters
+const char YES = 'y';  // indicates the user input to continue the game
+const char NO = 'n';  // indicates the user input to end the game
+const char NULL_TERMINATOR = '\0';  // used to identify empty array spaces
+const char UNDERSCORE = '_';  // used to indicate letters the user has not guessed yet
+const string FILE_NAME = "words.txt";  // specified file name for the list of words used
+const string SPACE = " ";  // used for formatting letters throughout the game
+
+int readFile(string fileName);
+void loadWords(string fileName, string wordArr[], int arrSize);
+char playGame(string keyWord);
+void displayPuzzle(char solvedChars[], int arrSize);
+char guessChar();
+bool updatePuzzle(char guess, string keyWord, char solvedChars[], int arrSize);
+bool usedChar(char usedChars[], int arrSize, char guess);
+int numLettersLeft(char solvedChars[], int arrSize);
+
+int main() {
+    srand(time(NULL));
+
+    cout << "Welcome to UMBC Word Guess" << endl;
+    // arrSize contains the int returned from readFile, the number of words 
+    // contained in the file FILE_NAME
+    int arrSize = readFile(FILE_NAME);
+    cout << arrSize << " words were imported." << endl;
+
+    string wordArr[arrSize];
+    // loadWords loads each word read in FILE_NAME into wordArr
+    loadWords(FILE_NAME, wordArr, arrSize);
+
+    char yesOrNo;
+    string keyWord;  // variable to hold the random word
+    // this do while loop continues playing the game until the user input indicates otherwise
+    do{
+        keyWord = wordArr[rand() % arrSize];
+        yesOrNo = playGame(keyWord);
+    }while(yesOrNo == YES);
+    cout << "Thanks for playing UMBC Word Guess!" << endl;
+  return 0;
+}
+
+/*
+** Name: readFile
+** PreCondition: none
+** PostCondition: returns the number of words listed in the file and 
+**                and confirms that the file exists
+*/
+int readFile(string fileName){
+    string word;  // used to read through each word in the file
+    int arrSize = 0;  // count in order to create an array of correct size
+    ifstream myFile (fileName);
+
+    if(myFile.is_open()){
+        cout << "Your file was imported!" << endl;
+        while(myFile >> word){
+            ++ arrSize;
+        }
+    }else{
+        cout << "Your file failed to open." << endl;
+    }
+
+    myFile.close();
+    return arrSize;
+}
+
+/*
+** Name: loadWords
+** PreCondition: fileName exists as a file, arrSize is large enough to hold each word
+**               read in from the file fileName
+** PostCondition: wordArr will contain strings of each word contained in the file fileName
+*/
+void loadWords(string fileName, string wordArr[], int arrSize){
+    ifstream myFile (fileName);
+    string word;  // used to read in each word in the file
+
+    int i = 0;
+    while(myFile >> word){
+        wordArr[i] = word;
+        ++ i;
+    }
+    myFile.close();
+}
+
+/*
+** Name: playGame
+** PreCondition: keyWord contains a string of letters and no symbols
+** PostCondition: returns char YES or NO to indicate if the user would like to 
+**                continue playing
+*/
+char playGame(string keyWord){
+    int numWrong = 0;  // counter for when the user guesses wrong
+    int wordLen = keyWord.length();  // stores the length of keyWord to use as length of solvedChars
+
+    char solvedChars[wordLen];  // array which holds the puzzle
+    // fills solvedChars with underscores
+    for(int j = 0; j < wordLen; j++){
+        solvedChars[j] = UNDERSCORE;
+    }
+    char usedChars[NUM_WRONG];  // array which holds wrong guesses
+    // fills userChars with null terminators
+    for(int j = 0; j < NUM_WRONG; j++){
+        usedChars[j] = NULL_TERMINATOR;
+    }
+
+    cout << "Ok. I am thinking of a word with " << wordLen << " letters." << endl;
+    // while loop continues as long as the user has not guessed the word and 
+    // has not used all guesses
+    while (numWrong != NUM_WRONG && numLettersLeft(solvedChars, wordLen) != 0){
+        // the next two lines display the status of the game to the user
+        cout << numLettersLeft(solvedChars, wordLen) << " letters remain." << endl;
+        displayPuzzle(solvedChars, wordLen);
+
+        char guess = guessChar();  // this char holds user input
+        bool usedGuess = usedChar(usedChars, NUM_WRONG, guess);
+        bool correctGuess = updatePuzzle(guess, keyWord, solvedChars, wordLen);
+
+        // accounts for a correct guess that has not been guessed before
+        if(correctGuess && !usedGuess){
+            usedChars[numWrong] = guess;
+        }
+        // accounts for a wrong guess that has not been guessed before
+        else if(!usedGuess){
+            cout << "There are no letters " << guess << " in the puzzle" << endl;
+            usedChars[numWrong] = guess;
+            ++numWrong;
+            cout << "You have " << (NUM_WRONG - numWrong) << " bad guesses left." << endl;
+        }
+        // accounts for a guess which has already been guessed
+        else{
+            ++numWrong;
+            cout << "The letter " << guess << " has already been guessed." << endl;
+            cout << "You have " << (NUM_WRONG - numWrong) << " bad guesses left." << endl;
+        }
+    }
+
+    // displays message if the word was not guessed before chances ran out
+    if(numWrong == NUM_WRONG){
+        cout << "Sorry, you lost." << endl << "The correct word was: " << keyWord << endl;
+    }
+    // displays message if the word was guessed
+    else{
+        cout << "Congrats, you won!" << endl << "The correct word was: " << keyWord << endl;
+    }
+    
+    char newGame;  // holds the YES or NO char from the user
+    do{
+        cout << "Another game? y/n" << endl;
+        cin >> newGame;
+    }while(newGame != YES && newGame != NO);
+    return newGame;
+}
+
+/*
+** Name: displayPuzzle
+** PreCondition: none
+** PostCondition: prints the puzzle so far, with spaces in between letters/underscores
+*/
+void displayPuzzle(char solvedChars[], int arrSize){
+    // the for loop iterates through the entire solvedChars array and prints each char
+    for(int i = 0; i < arrSize; i++){
+        cout << solvedChars[i] << SPACE;
+    }
+    cout << endl;
+}
+
+/*
+** Name: guessChar
+** PreCondition: none
+** PostCondition: returns the user input as a char
+*/
+char guessChar(){
+    char guess;
+    // this do while loop ensures that the user input is a lowercase letter
+    do{
+        cout << "What letter would you like to guess?" << endl;
+        cin >> guess;
+    }while(guess < ASCII_VAL);
+    return guess;
+}
+
+/*
+** Name: updatePuzzle
+** PreCondition: none
+** PostCondition: changes any letters in solvedChars that correspond with the user's guess
+**                and returns whether there was any change made to the solvedChars array
+**                
+*/
+bool updatePuzzle(char guess, string keyWord, char solvedChars[], int arrSize){
+    bool correctGuess = false;  // this bool will only change to true if a letter in keyWord 
+    // equals the guess made by the user
+    for(int i = 0; i < keyWord.length(); i++){
+        if(keyWord[i] == guess){
+            // switches the solvedChars empty value to the guessed char
+            solvedChars[i] = guess;
+            correctGuess = true;
+        }
+    }
+    return correctGuess;
+}
+
+/*
+** Name: usedChar
+** PreCondition: none
+** PostCondition: returns whether the guess existed in the userChars array
+*/
+bool usedChar(char usedChars[], int arrSize, char guess){
+    for(int i = 0; i < arrSize; i++){
+        // if statement to check if the guess exists in the usedChars array
+        if(usedChars[i] == guess){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+** Name: numLettersLeft
+** PreCondition: arrSize is greater than 0
+** PostCondition: returns the number of letters left for the user to guess
+*/
+int numLettersLeft(char solvedChars[], int arrSize){
+    int lettersLeft = 0;
+
+    // loop to count the number of spaces left
+    for(int i = 0; i < arrSize; i++){
+        if(solvedChars[i] == UNDERSCORE){
+            ++lettersLeft;
+        }
+    }
+    return lettersLeft;
+}
